17b07c47f164f772b48155bea7a4e5a12ae0da40
Frits Sweijen <tikk3r@users.noreply.github.com>
2025-12-02 14:15:15 +0000

    Print all cpu statistics

--- scripts/parse_toil_stats.py
+++ scripts/parse_toil_stats.py
@@ -7,12 +7,37 @@ with open(sys.argv[1]) as f:
 
 
 max_ram = 0
-max_name = ""
+max_ram_name = ""
+
+max_cpu = 0
+max_cpu_name = ""
+
+max_cpu_wait = 0
+max_cpu_wait_name = ""
+
+max_cpu_total = 0
+max_cpu_total_name = ""
 for name, job in data["job_types"].items():
     if job["max_memory"] > max_ram:
         max_ram = job["max_memory"]
-        max_name = name
+        max_ram_name = name
     if job["max_memory"] > 8e6:
         print(f"Job {name} used >8GB RAM -- {job['max_memory']/1e6:.2f} GB used")
 
-print(f"Max RAM of {max_ram/1e6}MB used by {max_name}")
+    if job["max_clock"] > max_cpu:
+        max_cpu = job["max_clock"]
+        max_cpu_name = name
+
+    if job["max_wait"] > max_cpu_wait:
+        max_cpu_wait = job["max_wait"]
+        max_cpu_wait_name = name
+
+    if (job["max_clock"] + job["max_wait"]) > max_cpu_total:
+        max_cpu_total = job["max_clock"] + job["max_wait"]
+        max_cpu_total_name = name
+
+print("\n== Summary ==")
+print(f"Max RAM of {max_ram/1e6:.3f}GB used by {max_ram_name}")
+print(f"Max CPU hours of {max_cpu/3600:.3f}h used by {max_cpu_name}")
+print(f"Max CPU hours of {max_cpu_wait/3600:.3f}h spent waiting by {max_cpu_wait_name}")
+print(f"Max CPU hours of {max_cpu_total/3600:.3f}h (used+wait) by {max_cpu_total_name}")
